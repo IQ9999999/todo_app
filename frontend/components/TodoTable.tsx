3c8ddf3a5593a8dcbff4c93ee12a2bda1238c813
IQ9999999 <otyuen001@gmail.com>
2023-06-25 15:06:41 +0800

    client present -> React-Apllo Hooks

--- frontend/components/TodoTable.tsx
+++ frontend/components/TodoTable.tsx
@@ -1,79 +1,80 @@
 import React, { useState } from "react";
-import { useQuery, useMutation, DocumentNode } from "@apollo/client";
-import { graphql } from "../graphql/generated/gql";
-
-const todosQueryDocument: DocumentNode = graphql(
-  "query GetTodos($sortDirection: String) {\n  todos(sortDirection: $sortDirection) {\n    id\n    title\n    description\n    completed\n  }\n}"
-);
-const createTodoGraph: DocumentNode = graphql(
-  "mutation CreateTodo($title: String!, $description: String!, $completed: Boolean!) {\n  createTodo(\n    input: {input: {title: $title, description: $description, completed: $completed}}\n  ) {\n    todo {\n      id\n      title\n      description\n      completed\n    }\n  }\n}"
-);
-const updateTodoGraph: DocumentNode = graphql(
-  "mutation UpdateTodo($id: ID!, $title: String!, $description: String, $completed: Boolean) {\n  updateTodo(\n    input: {id: $id, input: {title: $title, description: $description, completed: $completed}}\n  ) {\n    todo {\n      id\n      title\n      description\n      completed\n    }\n  }\n}"
-);
-const deleteTodoGraph: DocumentNode = graphql(
-  "mutation DeleteTodo($id: ID!) {\n  deleteTodo(input: {id: $id}) {\n    todo {\n      id\n      title\n      description\n      completed\n    }\n  }\n}"
-);
+import {
+  GetTodosQueryVariables,
+  useGetTodosQuery,
+  CreateTodoMutationVariables,
+  useCreateTodoMutation,
+  UpdateTodoMutationVariables,
+  useUpdateTodoMutation,
+  DeleteTodoMutationVariables,
+  useDeleteTodoMutation,
+} from "@/graphql/generated/graphql";
 
 function TodoTable() {
-  const [newTodo, setNewTodo] = useState({ title: "", description: "" });
-  const [sortDirection, setSortDirection] = useState("asc");
-  const { loading, error, data, refetch } = useQuery(todosQueryDocument, { variables: { sortDirection } });
+  const [sortDirection, setSortDirection] = useState<GetTodosQueryVariables["sortDirection"]>("asc");
+  const { loading, error, data, refetch } = useGetTodosQuery({
+    variables: { sortDirection },
+    fetchPolicy: "cache-and-network",
+    pollInterval: 5000,
+  });
+  const [newTodo, setNewTodo] = useState<CreateTodoMutationVariables>({
+    title: "",
+    description: "",
+    completed: false,
+  });
 
-  const [createTodo] = useMutation(createTodoGraph, {
-    onCompleted: () => {
-      refetch();
-    },
-  });
-  const [updateTodo] = useMutation(updateTodoGraph, {
-    onCompleted: () => {
-      refetch();
-    },
-  });
-  const [deleteTodo] = useMutation(deleteTodoGraph, {
-    onCompleted: () => {
-      refetch();
-    },
-  });
+  const [createTodoMutation] = useCreateTodoMutation();
+  const [updateTodoMutation] = useUpdateTodoMutation();
+  const [deleteTodoMutation] = useDeleteTodoMutation();
 
   const handleSaveTodo = async () => {
-    try {
-      await createTodo({
-        variables: {
-          title: newTodo.title,
-          description: newTodo.description,
-          completed: false,
-        },
+    const { data, errors } = await createTodoMutation({
+      variables: {
+        title: newTodo.title,
+        description: newTodo.description,
+        completed: newTodo.completed,
+      },
+    });
+    if (data) {
+      alert("Todo created successfully!");
+      setNewTodo({
+        title: "",
+        description: "",
+        completed: false,
       });
-      setNewTodo({ title: "", description: "" });
-    } catch (error) {
-      console.error("Error creating todo:", error);
+      refetch();
+      return;
     }
+    alert(errors);
   };
-  const handleUpdateTodo = async (id: string, title: string, description: string, completed: boolean) => {
-    try {
-      await updateTodo({
-        variables: {
-          id,
-          title,
-          description,
-          completed,
-        },
-      });
-    } catch (error) {
-      console.error("Error updating todo:", error);
+  const handleUpdateTodo = async ({ id, title, description, completed }: UpdateTodoMutationVariables) => {
+    const { data, errors } = await updateTodoMutation({
+      variables: {
+        id,
+        title,
+        description,
+        completed,
+      },
+    });
+    if (data) {
+      alert("Todo updated successfully!");
+      refetch();
+      return;
     }
+    alert(errors);
   };
-  const handleDeleteTodo = async (id: string) => {
-    try {
-      await deleteTodo({
-        variables: {
-          id,
-        },
-      });
-    } catch (error) {
-      console.error("Error deleting todo:", error);
+  const handleDeleteTodo = async ({ id }: DeleteTodoMutationVariables) => {
+    const { data, errors } = await deleteTodoMutation({
+      variables: {
+        id,
+      },
+    });
+    if (data) {
+      alert("Todo deleted successfully!");
+      refetch();
+      return;
     }
+    alert(errors);
   };
 
   const handleSortDirectionChange = () => {
@@ -84,6 +85,9 @@ function TodoTable() {
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
+  if (!data) return <p>No data</p>;
+
+  const { todos } = data;
 
   return (
     <table className="todo-table">
@@ -93,30 +97,39 @@ function TodoTable() {
             Title <button onClick={handleSortDirectionChange}>{sortDirection === "asc" ? "▲" : "▼"}</button>
           </th>
           <th>Description</th>
+          <th>Completed</th>
           <th>Action</th>
         </tr>
       </thead>
       <tbody>
-        {data.todos.map((todo: { id: string; title: string; description: string; completed: boolean }) => (
-          <tr key={todo.id}>
+        {todos.map(({ id, title, description, completed }) => (
+          <tr key={id}>
             <td>
               <input
                 type="text"
-                value={todo.title}
-                onChange={(e) => handleUpdateTodo(todo.id, e.target.value, todo.description, todo.completed)}
+                value={title}
+                onChange={(e) => handleUpdateTodo({ id, title: e.target.value, description, completed })}
                 className="todo-input"
               />
             </td>
             <td>
               <input
                 type="text"
-                value={todo.description}
-                onChange={(e) => handleUpdateTodo(todo.id, todo.title, e.target.value, todo.completed)}
+                value={description || ""}
+                onChange={(e) => handleUpdateTodo({ id, title, description: e.target.value, completed })}
                 className="todo-input"
               />
             </td>
             <td>
-              <button onClick={() => handleDeleteTodo(todo.id)} className="delete-button">
+              <input
+                type="checkbox"
+                checked={completed}
+                onChange={(e) => handleUpdateTodo({ id, title, description, completed: e.target.checked })}
+                className="todo-input"
+              />
+            </td>
+            <td>
+              <button onClick={() => handleDeleteTodo({ id })} className="delete-button">
                 Delete
               </button>
             </td>
@@ -136,6 +149,14 @@ function TodoTable() {
               type="text"
               value={newTodo.description}
               onChange={(e) => setNewTodo({ ...newTodo, description: e.target.value })}
+              className="todo-input"
+            />
+          </td>
+          <td>
+            <input
+              type="checkbox"
+              checked={newTodo.completed}
+              onChange={(e) => setNewTodo({ ...newTodo, completed: e.target.checked })}
               className="todo-input"
             />
           </td>
